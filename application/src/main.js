ca676b341a53cdd4131e6e7be9731866b73eaf76
Matt Leach <MLeach@symprod.com>
2017-11-09 17:45:35 -0500

    changes

--- application/src/main.js
+++ application/src/main.js
@@ -4,6 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import VueCookie from 'vue-cookie'
+import Vuetify from 'vuetify'
+
+Vue.use(VueCookie)
+Vue.use(Vuetify)
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
